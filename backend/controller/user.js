06d7450c8902947014263a1a3734de602de7ff04
tejasvi raj <tejasviraj8686@gmail.com>
2024-02-14 08:32:35 +0530

    adde

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -6,32 +6,29 @@ const { upload } = require("../multer");
 const ErrorHandler = require("../utils/ErrorHandler");
 
 router.post("/create-user", upload.single("file"), async (req, res, next) => {
-  try {
-    const { name, email, password } = req.body;
-    const userEmail = await User.findOne({ email });
+  const { name, email, password } = req.body;
+  const userEmail = await User.findOne({ email });
 
-    if (userEmail) {
-      return next(new ErrorHandler("User already exists", 400));
-    }
-
-    const filename = req.file.filename;
-    const fileUrl = path.join(filename);
-
-    const user = {
-      name: name,
-      email: email,
-      password: password,
-      avatar: fileUrl,
-    };
-
-    const newUser = await User.create(user);
-    res.status(201).json({
-      success: true,
-      newUser,
-    });
-  } catch (error) {
-    return next(new ErrorHandler(error.message));
+  if (userEmail) {
+    return next(new ErrorHandler("User already exists", 400));
   }
+
+  const filename = req.file.filename;
+  const fileUrl = path.join(filename);
+
+  const user = {
+    name: name,
+    email: email,
+    password: password,
+    avatar: fileUrl,
+  };
+
+  console.log(user);
+  const newUser = await User.create(user);
+  res.status(201).json({
+    sucess: true,
+    newUser,
+  });
 });
 
 module.exports = router;
